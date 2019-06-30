a8ac9426edd49bb27430e57e09c6b2f8896b2ad3
zkangHUST <1473345713@qq.com>
2019-06-30 23:33:25 +0800

    添加插入排序

---
+++ sort/insert-sort.c
@@ -0,0 +1,16 @@
+// insert-sort
+void insert_sort(int nums[], int len)
+{
+    int i, j, key;
+
+    for(j = 1; j < len; j++)
+    {
+        key = nums[j];
+        i = j-1;
+        while(i >= 0 && nums[i] > key) {
+            nums[i + 1] = nums[i];
+            i = i-1;
+        }
+        nums[i + 1] = key;
+    }
+}
